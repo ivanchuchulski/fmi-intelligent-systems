807cbcae7c7af892c23cd2e7a0ad3f3f5199d583
Ivan Chuchulski <zake6@abv.bg>
2020-11-06 12:33:56 +0200

    refactor test names, add 5x5 tests

--- hw-01-tiles-puzzle/test/IDAStarTest.java
+++ hw-01-tiles-puzzle/test/IDAStarTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class IDAStarTest {
     @Test
-    public void test1() {
+    public void test_3x3_1() {
         int[][] initialState = buildBoardFromString("123456078");
         int[][] goalState = generateDefualtGoalState(initialState);
 
@@ -20,7 +20,7 @@ class IDAStarTest {
     }
 
     @Test
-    public void test2() {
+    public void test_3x3_2() {
         int[][] initialState = buildBoardFromString("123745086");
         int[][] goalState = generateDefualtGoalState(initialState);
 
@@ -35,7 +35,7 @@ class IDAStarTest {
     }
 
     @Test
-    public void test3() {
+    public void test_3x3_3() {
         int[][] initialState = buildBoardFromString("123480765");
         int[][] goalState = generateDefualtGoalState(initialState);
 
@@ -50,7 +50,7 @@ class IDAStarTest {
     }
 
     @Test
-    public void test4() {
+    public void test_3x3_4() {
         int[][] initialState = buildBoardFromString("413726580");
         int[][] goalState = generateDefualtGoalState(initialState);
 
@@ -65,7 +65,7 @@ class IDAStarTest {
     }
 
     @Test
-    public void test5() {
+    public void test_3x3_5() {
         int[][] initialState = buildBoardFromString("162530478");
         int[][] goalState = generateDefualtGoalState(initialState);
 
@@ -80,7 +80,7 @@ class IDAStarTest {
     }
 
     @Test
-    public void test6() {
+    public void test_3x3_6() {
         int[][] initialState = buildBoardFromString("512630478");
         int[][] goalState = generateDefualtGoalState(initialState);
 
@@ -95,7 +95,7 @@ class IDAStarTest {
     }
 
     @Test
-    public void test7() {
+    public void test_3x3_7() {
         int[][] initialState = buildBoardFromString("126350478");
         int[][] goalState = generateDefualtGoalState(initialState);
 
@@ -110,7 +110,7 @@ class IDAStarTest {
     }
 
     @Test
-    public void test8() {
+    public void test_3x3_8() {
         int[][] initialState = buildBoardFromString("436871052");
         int[][] goalState = generateDefualtGoalState(initialState);
 
@@ -125,7 +125,7 @@ class IDAStarTest {
     }
 
     @Test
-    public void test9() {
+    public void test_3x3_9() {
         int[][] initialState = buildBoardFromString("503284671");
         int[][] goalState = generateDefualtGoalState(initialState);
 
@@ -140,7 +140,7 @@ class IDAStarTest {
     }
 
     @Test
-    public void test10() {
+    public void test_3x3_10() {
         int[][] initialState = buildBoardFromString("874320651");
         int[][] goalState = generateDefualtGoalState(initialState);
 
@@ -156,7 +156,7 @@ class IDAStarTest {
     }
 
     @Test
-    public void test11() {
+    public void test_3x3_11() {
         // has 2 solutions
         int[][] initialState = buildBoardFromString("876543021");
         int[][] goalState = generateDefualtGoalState(initialState);
@@ -173,7 +173,7 @@ class IDAStarTest {
     }
 
     @Test
-    public void test12() {
+    public void test_3x3_12() {
         // has 2 solutions
         int[][] initialState = buildBoardFromString("876543210");
         int[][] goalState = generateDefualtGoalState(initialState);
@@ -191,7 +191,7 @@ class IDAStarTest {
     // 4x4 tests
 
     @Test
-    public void test13() {
+    public void test_4x4_1() {
         int[][] initialState = buildBoardFromArray(new int[]{1, 2, 3, 4, 5, 6, 7, 0, 9, 10, 11, 8, 13, 14, 15, 12});
         int[][] goalState = generateDefualtGoalState(initialState);
 
@@ -206,7 +206,7 @@ class IDAStarTest {
     }
 
     @Test
-    public void test14() {
+    public void test_4x4_2() {
         int[][] initialState = buildBoardFromArray(new int[]{1, 2, 3, 4, 5, 0, 7, 8, 9, 6, 11, 12, 13, 10, 14, 15});
         int[][] goalState = generateDefualtGoalState(initialState);
 
@@ -221,7 +221,7 @@ class IDAStarTest {
     }
 
     @Test
-    public void test15() {
+    public void test_4x4_3() {
         int[][] initialState = buildBoardFromArray(new int[]{1, 6, 2, 4, 5, 0, 3, 8, 9, 10, 7, 11, 13, 14, 15, 12});
         int[][] goalState = generateDefualtGoalState(initialState);
 
@@ -236,7 +236,7 @@ class IDAStarTest {
     }
 
     @Test
-    public void test16() {
+    public void test_4x4_4() {
         int[][] initialState = buildBoardFromArray(new int[]{1, 2, 4, 12, 5, 6, 3, 0, 9, 10, 8, 7, 13, 14, 11, 15});
         int[][] goalState = generateDefualtGoalState(initialState);
 
@@ -251,7 +251,7 @@ class IDAStarTest {
     }
 
     @Test
-    public void test17() {
+    public void test_4x4_5() {
         int[][] initialState = buildBoardFromArray(new int[]{1, 2, 8, 3, 5, 11, 6, 4, 0, 10, 7, 12, 9, 13, 14, 15});
         int[][] goalState = generateDefualtGoalState(initialState);
 
@@ -266,7 +266,7 @@ class IDAStarTest {
     }
 
     @Test
-    public void test18() {
+    public void test_4x4_6() {
         int[][] initialState = buildBoardFromArray(new int[]{2, 5, 3, 4, 1, 7, 11, 8, 9, 6, 0, 12, 13, 14, 15, 10});
         int[][] goalState = generateDefualtGoalState(initialState);
 
@@ -281,7 +281,7 @@ class IDAStarTest {
     }
 
     @Test
-    public void test19() {
+    public void test_4x4_7() {
         int[][] initialState = buildBoardFromArray(new int[]{1, 4, 8, 3, 7, 2, 10, 11, 5, 6, 0, 15, 9, 13, 14, 12});
         int[][] goalState = generateDefualtGoalState(initialState);
 
@@ -296,7 +296,7 @@ class IDAStarTest {
     }
 
     @Test
-    public void test20() {
+    public void test_4x4_8() {
         int[][] initialState = buildBoardFromArray(new int[]{2, 5, 4, 7, 9, 1, 3, 8, 11, 10, 0, 6, 14, 13, 15, 12});
         int[][] goalState = generateDefualtGoalState(initialState);
 
@@ -311,7 +311,7 @@ class IDAStarTest {
     }
 
     @Test
-    public void test21() {
+    public void test_4x4_9() {
         int[][] initialState = buildBoardFromArray(new int[]{9, 5, 8, 3, 6, 0, 10, 11, 2, 1, 14, 7, 13, 15, 12, 4});
         int[][] goalState = generateDefualtGoalState(initialState);
 
@@ -326,7 +326,7 @@ class IDAStarTest {
     }
 
     @Test
-    public void test22() {
+    public void test_4x4_10() {
         int[][] initialState = buildBoardFromArray(new int[]{2, 6, 4, 8, 1, 10, 7, 3, 5, 13, 11, 15, 12, 14, 9, 0});
         int[][] goalState = generateDefualtGoalState(initialState);
 
@@ -341,7 +341,7 @@ class IDAStarTest {
     }
 
     @Test
-    public void test23() {
+    public void test_4x4_11() {
         int[][] initialState = buildBoardFromArray(new int[]{14, 5, 11, 6, 9, 0, 4, 3, 15, 1, 7, 12, 8, 2, 13, 10});
         int[][] goalState = generateDefualtGoalState(initialState);
 
@@ -356,24 +356,39 @@ class IDAStarTest {
     }
 
     @Test
-    @Disabled
-    public void test24() {
-        int[][] initialState = buildBoardFromArray(new int[]{13, 11, 9, 3, 14, 7, 1, 4, 0, 5, 10, 12, 15, 2, 6, 8});
+    public void test_4x4_12() {
+        int[][] initialState = buildBoardFromArray(new int[]{6, 8, 4, 12, 7, 15, 10, 14, 0, 3, 2, 11, 1, 5, 9, 13 });
         int[][] goalState = generateDefualtGoalState(initialState);
 
         final int steps = 46;
-        final String moves = "DDLUUURDDLLDRUULURRDDDLULULURRDDRULDLDLUURRULL";
+        final String moves = "LLDRURDLULLDRRDRUUULLLDDDRURRUULLLDDRRDRUUULLL";
 
         IDAStar idaStar = new IDAStar(initialState, goalState);
         idaStar.findSolution();
 
         assertEquals(steps, idaStar.getNumberOfStepsToSolution());
-        // assertEquals(moves, idaStar.getMoves());
+//         assertEquals(moves, idaStar.getMoves());
     }
 
     @Test
     @Disabled
-    public void test25() {
+    public void test_4x4_13() {
+        int[][] initialState = buildBoardFromArray(new int[]{13, 11, 9, 3, 14, 7, 1, 4, 0, 5, 10, 12, 15, 2, 6, 8});
+        int[][] goalState = generateDefualtGoalState(initialState);
+
+        final int steps = 46;
+        final String moves = "DLLDRURDLUUURDDLULDDRUULULDDRUURRDLDLLURDDLUUU";
+
+        IDAStar idaStar = new IDAStar(initialState, goalState);
+        idaStar.findSolution();
+
+        assertEquals(steps, idaStar.getNumberOfStepsToSolution());
+//         assertEquals(moves, idaStar.getMoves());
+    }
+
+    @Test
+    @Disabled
+    public void test_4x4_14() {
         int[][] initialState = buildBoardFromArray(new int[]{2, 9, 3, 5, 8, 11, 12, 7, 15, 4, 0, 13, 6, 1, 10, 14});
         int[][] goalState = generateDefualtGoalState(initialState);
 
@@ -389,11 +404,39 @@ class IDAStarTest {
 
     @Test
     @Disabled
-    public void test26() {
+    public void test_4x4_15() {
         int[][] initialState = buildBoardFromArray(new int[]{0, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1});
         int[][] goalState = generateDefualtGoalState(initialState);
 
         final int steps = 78;
+
+        IDAStar idaStar = new IDAStar(initialState, goalState);
+        idaStar.findSolution();
+
+        assertEquals(steps, idaStar.getNumberOfStepsToSolution());
+    }
+
+    // 5x5 tests
+    @Test
+    public void test_5x5_1() {
+        int[][] initialState = buildBoardFromArray(new int[]{1, 2, 3, 4, 5, 12, 6, 8, 9, 10, 0, 7, 13, 19, 14, 11, 16, 17, 18, 15, 21, 22, 23, 24, 20});
+        int[][] goalState = generateDefualtGoalState(initialState);
+
+        final int steps = 12;
+
+        IDAStar idaStar = new IDAStar(initialState, goalState);
+        idaStar.findSolution();
+
+        assertEquals(steps, idaStar.getNumberOfStepsToSolution());
+    }
+
+    @Test
+    public void test_5x5_2() {
+        int[][] initialState = buildBoardFromArray(new int[]{6, 7, 5, 10, 15, 3, 2, 8, 0, 4, 1, 12, 20, 13, 9, 11, 16, 19, 14, 24, 21, 17, 22, 18, 23
+        });
+        int[][] goalState = generateDefualtGoalState(initialState);
+
+        final int steps = 44;
 
         IDAStar idaStar = new IDAStar(initialState, goalState);
         idaStar.findSolution();
