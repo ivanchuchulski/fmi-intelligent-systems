b021572517e717b47521a76c18614489f2463af9
Ivan Chuchulski <zake6@abv.bg>
2020-11-10 18:37:10 +0200

    cleanup, refactor

--- hw-02-n-queens/src/MyNQueens.java
+++ hw-02-n-queens/src/MyNQueens.java
@@ -1,20 +1,35 @@
 import java.util.*;
 
 public class MyNQueens {
-    public static final Random random = new Random();
+    public static Random random;
 
     private final int size;
-    private int[] queens;
+    private final int[] queens;
 
-    private int[] rowConfl;
-    private int[] mainDiagonalConflicts;
-    private int[] secondaryDiagonalConflicts;
+    private final int[] rowConflicts;
+    private final int[] mainDiagonalConflicts;
+    private final int[] secondaryDiagonalConflicts;
 
-    private List<Conflict> minConflictQueens = new ArrayList<>();
-    private List<Conflict> maxConflictQueens = new ArrayList<>();
+    private final List<Conflict> rowsWithMinConflicts;
+    private final List<Conflict> colsWithMaxConflicts;
 
     public MyNQueens(int size) {
         this.size = size;
+
+        queens = new int[size];
+
+        rowConflicts = new int[size];
+        mainDiagonalConflicts = new int[2 * size - 1];
+        secondaryDiagonalConflicts = new int[2 * size - 1];
+
+        rowsWithMinConflicts = new ArrayList<>();
+        colsWithMaxConflicts = new ArrayList<>();
+
+        int value = 0;
+        for (int i = 0; i < size; i++) {
+            queens[i] = value++;
+        }
+
         init();
     }
 
@@ -22,6 +37,8 @@ public class MyNQueens {
         boolean conflictsResolved = false;
         int currentIterations = 0;
         final int MAX_ITERATIONS = size * 2;
+
+        init();
 
         while (currentIterations++ < MAX_ITERATIONS) {
             int colWithMaxConfl = getColwWithMaxConflicts();
@@ -34,120 +51,104 @@ public class MyNQueens {
             int prevRow = queens[colWithMaxConfl];
             int nextRow = getRowWithMinConflicts(colWithMaxConfl);
 
-            queens[colWithMaxConfl] = nextRow;
-
-            rowConfl[prevRow]--;
-            mainDiagonalConflicts[getMainDiagonalIndexForCell(prevRow, colWithMaxConfl)]--;
-            secondaryDiagonalConflicts[getSecDiagonalIndexForCell(prevRow, colWithMaxConfl)]--;
-
-            rowConfl[nextRow]++;
-            mainDiagonalConflicts[getMainDiagonalIndexForCell(nextRow, colWithMaxConfl)]++;
-            secondaryDiagonalConflicts[getSecDiagonalIndexForCell(nextRow, colWithMaxConfl)]++;
+            moveQueen(colWithMaxConfl, prevRow, nextRow);
         }
 
         if (!conflictsResolved) {
-            init();
             resolveConflicts();
         }
 
-
-//        print();
+//        printSolutionBoard();
     }
 
+
     private void init() {
-        queens = new int[size];
-        rowConfl = new int[size];
+        random = new Random();
+        shuffleArray(queens);
 
-        mainDiagonalConflicts = new int[2 * size - 1];
-        secondaryDiagonalConflicts = new int[2 * size - 1];
-
-        List<Integer> shuffledRows = new ArrayList<>(size);
-        int value = 0;
-
-        for (int i = 0; i < size; i++) {
-            shuffledRows.add(value++);
-        }
-
-        Collections.shuffle(shuffledRows);
+        Arrays.fill(rowConflicts, 0);
+        Arrays.fill(mainDiagonalConflicts, 0);
+        Arrays.fill(secondaryDiagonalConflicts, 0);
 
         for (int col = 0; col < size; col++) {
-            int currentRow = shuffledRows.get(col);
+            int currentRow = queens[col];
 
-            queens[col] = currentRow;
-
-            rowConfl[currentRow]++;
+            rowConflicts[currentRow]++;
             mainDiagonalConflicts[getMainDiagonalIndexForCell(currentRow, col)]++;
             secondaryDiagonalConflicts[getSecDiagonalIndexForCell(currentRow, col)]++;
         }
     }
 
     private int getColwWithMaxConflicts() {
-        maxConflictQueens.clear();
+        colsWithMaxConflicts.clear();
 
         for (int col = 0; col < size; ++col) {
             int row = queens[col];
 
             int candidate =
-                    rowConfl[row] +
-                            mainDiagonalConflicts[size - 1 - row + col] +
-                            secondaryDiagonalConflicts[col + row] - 3;
+                    rowConflicts[row] +
+                            mainDiagonalConflicts[getMainDiagonalIndexForCell(row, col)] +
+                            secondaryDiagonalConflicts[getSecDiagonalIndexForCell(row, col)] - 3;
 
-            if (maxConflictQueens.isEmpty() || maxConflictQueens.get(0).value == candidate) {
-                maxConflictQueens.add(new Conflict(col, candidate));
+            if (colsWithMaxConflicts.isEmpty() || colsWithMaxConflicts.get(0).value == candidate) {
+                colsWithMaxConflicts.add(new Conflict(col, candidate));
             }
             else {
-                if (maxConflictQueens.get(0).value < candidate) {
-                    maxConflictQueens.clear();
-                    maxConflictQueens.add(new Conflict(col, candidate));
+                if (colsWithMaxConflicts.get(0).value < candidate) {
+                    colsWithMaxConflicts.clear();
+                    colsWithMaxConflicts.add(new Conflict(col, candidate));
                 }
             }
         }
 
-        if (maxConflictQueens.get(0).value == 0) {
+        if (colsWithMaxConflicts.get(0).value == 0) {
             return -1;
         }
 
 
-        return maxConflictQueens.get(random.nextInt(maxConflictQueens.size())).place;
+        return colsWithMaxConflicts.get(random.nextInt(colsWithMaxConflicts.size())).place;
     }
 
     private int getRowWithMinConflicts(int colWithMaxConfl) {
-        minConflictQueens.clear();
+        rowsWithMinConflicts.clear();
 
         for (int row = 0; row < size; ++row) {
-            // works with -3 or without it
             int candidate =
-                    rowConfl[row] +
-                    mainDiagonalConflicts[getMainDiagonalIndexForCell(row, colWithMaxConfl)] +
-                    secondaryDiagonalConflicts[getSecDiagonalIndexForCell(row, colWithMaxConfl)];
+                    rowConflicts[row] +
+                            mainDiagonalConflicts[getMainDiagonalIndexForCell(row, colWithMaxConfl)] +
+                            secondaryDiagonalConflicts[getSecDiagonalIndexForCell(row, colWithMaxConfl)];
 
             if (row == queens[colWithMaxConfl]) {
                 candidate -= 3;
             }
 
-            if (minConflictQueens.isEmpty() || minConflictQueens.get(0).value == candidate) {
-                minConflictQueens.add(new Conflict(row, candidate));
+            if (rowsWithMinConflicts.isEmpty() || rowsWithMinConflicts.get(0).value == candidate) {
+                rowsWithMinConflicts.add(new Conflict(row, candidate));
             }
             else {
-                if (minConflictQueens.get(0).value > candidate) {
-                    minConflictQueens.clear();
-                    minConflictQueens.add(new Conflict(row, candidate));
+                if (rowsWithMinConflicts.get(0).value > candidate) {
+                    rowsWithMinConflicts.clear();
+                    rowsWithMinConflicts.add(new Conflict(row, candidate));
                 }
             }
         }
 
-        return minConflictQueens.get(random.nextInt(minConflictQueens.size())).place;
+        return rowsWithMinConflicts.get(random.nextInt(rowsWithMinConflicts.size())).place;
     }
 
+    private void moveQueen(int colWithMaxConfl, int prevRow, int nextRow) {
+        queens[colWithMaxConfl] = nextRow;
+
+        rowConflicts[prevRow]--;
+        mainDiagonalConflicts[getMainDiagonalIndexForCell(prevRow, colWithMaxConfl)]--;
+        secondaryDiagonalConflicts[getSecDiagonalIndexForCell(prevRow, colWithMaxConfl)]--;
+
+        rowConflicts[nextRow]++;
+        mainDiagonalConflicts[getMainDiagonalIndexForCell(nextRow, colWithMaxConfl)]++;
+        secondaryDiagonalConflicts[getSecDiagonalIndexForCell(nextRow, colWithMaxConfl)]++;
+    }
 
     private int getMainDiagonalIndexForCell(int row, int col) {
-//        if (col - row > 0) {
-//            return col - row;
-//        }
-//        else {
-//            return Math.abs(col - row) + this.size - 1;
-//        }
-
         return col - row + this.size - 1;
     }
 
@@ -155,7 +156,18 @@ public class MyNQueens {
         return row + col;
     }
 
-    public void print() {
+//     source : https://stackoverflow.com/a/1520212/9127495
+    private void shuffleArray(int[] arr) {
+        for (int i = arr.length - 1; i > 0; i--) {
+            int index = random.nextInt(i + 1);
+            // Simple swap
+            int a = arr[index];
+            arr[index] = arr[i];
+            arr[i] = a;
+        }
+    }
+
+    public void printSolutionBoard() {
         System.out.println("finished : all conflicts resolved");
 
         System.out.println(Arrays.toString(queens));
